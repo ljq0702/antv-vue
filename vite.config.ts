fb3bcbb3d8073c072afcf43ff52b49d758f92302
xiaoli <1063080020@qq.com>
2024-05-31 22:11:59 +0800

    提交新分支

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/antv-vue',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      "@": '/src'
+    }
+  }
 })
