cb6b908d6db0dcc94e176f7b5e678fb0c2aa0338
xiaoli <1063080020@qq.com>
2024-05-31 23:39:35 +0800

    提交打包

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/antv-vue',
+  base: '',
   publicDir: 'public',
   build: {
     outDir: 'dist'
   },
   resolve: {
     alias: {
-      "@": '/src'
+      "@": 'src'
     }
   }
 })
